685f4b0aade5ee6efb757c8e217bc03f71c22a96
Canwdev <c2097@live.com>
2022-08-12 22:32:38 +0800

    frontend eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,24 @@
 module.exports = {
-  parser: 'vue-eslint-parser',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    // project: 'tsconfig.json',
-    // tsconfigRootDir: __dirname,
-    parser: '@typescript-eslint/parser',
-    ecmaVersion: 2020,
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'plugin:vue/vue3-essential',
   ],
   root: true,
   env: {
-    browser: true,
-    es2021: true,
     node: true,
   },
-  // ignorePatterns: [".eslintrc.js"],
+  ignorePatterns: ['.eslintrc.js', './frontend/**/*'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
-}
+};
