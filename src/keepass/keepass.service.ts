d41cecc7e35a541f148cff7d76e3f02ea4a44c44
Canwdev <c2097@live.com>
2022-08-14 11:01:50 +0800

    db config load

--- src/keepass/keepass.service.ts
+++ src/keepass/keepass.service.ts
@@ -1,8 +1,12 @@
 import {Injectable} from '@nestjs/common'
 import {KdbxHelper, KdbxOpenOptions} from './enum/kdbx'
 import * as Path from 'path'
+import * as Fs from 'fs-extra'
 
-const testPath = Path.join(__dirname, '../../test/databases')
+type JsonConfig = {
+  isRelativeBase: boolean // 当前配置文件目录是否为数据库和密钥都相对根目录
+  kdbxOpenOptions: KdbxOpenOptions
+}
 
 @Injectable()
 export class KeepassService {
@@ -10,11 +14,23 @@ export class KeepassService {
 
   constructor() {
     this.kdbxHelper = new KdbxHelper()
-    // this.openDatabase({
-    //   dbPath: Path.join(testPath, 'test[123456].kdbx'),
-    //   password: '123456',
-    //   keyPath: Path.join(testPath, 'test[123456].key'),
-    // })
+    this.loadDbConfig().then((openOptions) => {
+      this.openDatabase(openOptions)
+    })
+  }
+
+  async loadDbConfig(): Promise<KdbxOpenOptions> {
+    const configPath =
+      process.env.DB_CONFIG_JSON || Path.join(__dirname, '../../test/db-config.json')
+    const config: JsonConfig = JSON.parse(await Fs.readFile(configPath, 'utf8'))
+
+    const kdbxOpenOptions = config.kdbxOpenOptions
+    if (config.isRelativeBase) {
+      const basePath = Path.dirname(configPath)
+      kdbxOpenOptions.dbPath = Path.join(basePath, kdbxOpenOptions.dbPath)
+      kdbxOpenOptions.keyPath = Path.join(basePath, kdbxOpenOptions.keyPath)
+    }
+    return kdbxOpenOptions
   }
 
   openDatabase(options: KdbxOpenOptions) {
