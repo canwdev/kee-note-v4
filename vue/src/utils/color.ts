776369762d3c7efc6797bd12e436e90060c3ff0d
canwdev <c2097@live.com>
2024-01-06 22:35:46 +0800

    update

--- vue/src/utils/color.ts
+++ vue/src/utils/color.ts
@@ -1,3 +1,9 @@
+/**
+ * Converts a hexadecimal color code to an RGB object.
+ *
+ * @param {string} hex - The hexadecimal color code to convert.
+ * @return {object|null} An object representing the RGB values {r, g, b} of the color.
+ */
 export function hexToRgb(hex) {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
   return result
@@ -9,9 +15,22 @@ export function hexToRgb(hex) {
     : null
 }
 
+/**
+ * Converts RGB color values to a hexadecimal color string.
+ *
+ * @param {number} r - The red color value (0-255).
+ * @param {number} g - The green color value (0-255).
+ * @param {number} b - The blue color value (0-255).
+ * @return {string} The hexadecimal color string representation.
+ */
 export function rgbToHex(r, g, b) {
   return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
 }
 
+/**
+ * Returns a boolean value indicating whether the system is in dark mode.
+ *
+ * @return {boolean} A boolean value indicating whether the system is in dark mode.
+ */
 export const getSystemIsDarkMode = () =>
   window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
